b9f8dd48cf4a596f47c03fcd02dbeed88c6c566a
Malu <mlfaa@hotmail.com>
2020-10-19 17:16:13 -0300

    Modify huffman tree
    Co-authored-by: Maria Luiza <mlfaa@hotmail.com>
    Co-authored-by: Felipe Hargreaves <fhargreaves00@gmail.com>

--- huffman.py
+++ huffman.py
@@ -1,3 +1,4 @@
+import heapq
 # Huffman Coding in python
 
 # Creating tree nodes
@@ -13,12 +14,24 @@ class NodeTree(object):
     def nodes(self):
         return (self.left, self.right)
 
-    def __str__(self, level=0):
-        ret = "\t"*level+repr(self.value)+"\n"
-        for child in self.children():
-            ret += child.__str__(level+1)
-        return ret
+    # def __str__(self, level=0):
+    #     ret = "\t"*level+repr(self.value)+"\n"
+    #     for child in self.children():
+    #         ret += child.__str__(level+1)
+    #     return ret
 
+    def print_tree(self):
+        if type(self.left) is str:
+            print('=======LEFT=============')
+            print(self.left)
+        else: 
+            self.left.print_tree()
+
+        if type(self.right) is str:
+            print('========RIGHT===========')
+            print(self.right)
+        else: 
+            self.right.print_tree()
 
 # Main function implementing huffman coding
 def huffman_code_tree(node, left=True, binString=''):
@@ -44,33 +57,41 @@ def get_frequencies(string):
     return freq
 
 
-def encode(nodes):
-    while len(nodes) > 1:
-        (key1, c1) = nodes[-1]
-        (key2, c2) = nodes[-2]
-        nodes = nodes[:-2]
-        node = NodeTree(key1, key2)
-        nodes.append((node, c1 + c2))
-
-        nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
-
-    return nodes
-
 def solve(string, socketio):
-    frequencies = get_frequencies(string)
+    nodes = get_frequencies(string)
+    heap = [[weight, [symbol, weight, '']] for symbol, weight in nodes]
+    heapq.heapify(heap)
 
-    emit_data = []
-    for frequency in frequencies:
-        emit_data.append({
-            'letter': frequency[0],
-            'frequency': frequency[1],
-        })
-    socketio.emit('update', {'nodes': emit_data})
+    while len(heap) > 1:
+        lo = heapq.heappop(heap)
+        hi = heapq.heappop(heap)
+        for pair in lo[1:]:
+            pair[2] = '0' + pair[2]
+        for pair in hi[1:]:
+            pair[2] = '1' + pair[2]
 
-    nodes = encode(frequencies)
-    huffmanCode = huffman_code_tree(nodes[0][0])
+        heapq.heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
+        socketio.emit('update', {'nodes': heap})
 
-    print(' Char | Huffman code ')
-    print('----------------------')
-    for (char, frequency) in freq:
-        print(' %-4r |%12s' % (char, huffmanCode[char]))
+    return sorted(heapq.heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
+
+# def solve(string, socketio):
+    
+
+    # emit_data = []
+    # for frequency in frequencies:
+    #     emit_data.append({
+    #         'letter': frequency[0],
+    #         'frequency': frequency[1],
+    #     })
+    # socketio.emit('update', {'nodes': emit_data})
+
+    # nodes = encode(frequencies)
+    # print(nodes)
+    # huffmanCode = huffman_code_tree(nodes[0][0])
+
+    # nodes[0][0].print_tree()
+    # print(' Char | Huffman code ')
+    # print('----------------------')
+    # for (char, frequency) in frequencies:
+    #     print(' %-4r |%12s' % (char, huffmanCode[char]))
